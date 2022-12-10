8f29eeacfd56042501836af3606b796c552816c2
Neyli13 <93058901+Neyli13@users.noreply.github.com>
2022-12-09 20:27:31 -0600

    Add files via upload

---
+++ Dispositivos de Backups.sql
@@ -0,0 +1,21 @@
+﻿USE AdventureWorks2019
+go
+
+use master
+go
+
+EXEC sp_addumpdevice 'Disk', 'DEV_AdventureWorks',
+'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\Backup\DEV_AdventureWorks.bak';
+GO
+
+SELECT * FROM sys.backup_devices
+GO
+
+BACKUP DATABASE AdventureWorks2019
+TO DEV_AdventureWorks
+WITH FORMAT, INIT, NAME = N'AdventureWorks Full Backup'
+
+RESTORE HEADERONLY FROM DEV_AdventureWorks
+
+EXEC�sp_dropdevice�'AdventureWorksDevice',�'delfile' 
+ GO
